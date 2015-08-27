bbdb373db7b4b2e1568fdbc2497f376b45588c08
Karan Thakkar <karanjthakkar@gmail.com>
2015-08-27 19:46:53 +0530

    Fix CSP and cross origin request

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: '//localhost:3000'
+  host: '//localhost:3000',
+  ajax(url, method, hash) {
+    hash = hash || {};
+    hash.crossDomain = true;
+    hash.xhrFields = { withCredentials: true };
+    return this._super(url, method, hash);
+  }
 });
