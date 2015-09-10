713223bf987cefa67e48e69dcceb5b85546d5c61
Karan Thakkar <karanjthakkar@gmail.com>
2015-09-11 00:25:40 +0530

    Added loading hbs

---
+++ tests/unit/routes/app/profile/loading-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:app/profile/loading', 'Unit | Route | app/profile/loading', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
