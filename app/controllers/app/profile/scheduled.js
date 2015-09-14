f17e68d119b1dd021bcdde6bef35526539fda87b
Karan Thakkar <karanjthakkar@gmail.com>
2015-09-15 00:50:18 +0530

    Fixes background for long lists on profile page. Handle various conditions for list

--- app/controllers/app/profile/scheduled.js
+++ app/controllers/app/profile/scheduled.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   user: Ember.inject.service(),
+  isAnalysedOnce: Ember.computed.gt('user.data.total_tweets_analysed', 0),
+  isPostedOnce: Ember.computed.gt('user.data.total_tweets_posted', 0),
   isAccountCreatedTimeMoreThanHour: Ember.computed(function() {
     var created = this.get('user.data.created_at'),
       lastCron = this.get('user.data.last_cron_run_time');
